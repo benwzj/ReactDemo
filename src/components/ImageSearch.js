452ae846e900679ec878a72eab32ecd763666caf
benwzj <57896457+benwzj@users.noreply.github.com>
2024-07-29 23:49:10 +1000

    pic ui

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import '../css/ImageList.css';
 import '../css/SearchBar.css';
 import { RxMagnifyingGlass } from "react-icons/rx";
@@ -22,17 +22,11 @@ function ImageItem({ image }) {
 
 export function SearchBar({ onSubmit, term }) {
   const [Input, setInput] = useState(term);
-  const [displayedTerm, setDisplayedTerm] = useState(term);
-
-  useEffect(()=>{
-    setDisplayedTerm (term);
-  },[term]); 
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
     onSubmit(Input);
-    setDisplayedTerm (Input);
     setInput ('');
   };
 
@@ -44,7 +38,7 @@ export function SearchBar({ onSubmit, term }) {
     
       <form onSubmit={handleFormSubmit}>
         <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-          <div className="relative flex flex-1 flex-shrink-0">
+          <div className="relative flex flex-1 ">
             <label htmlFor="search" className="sr-only">
               Search
             </label>
@@ -55,14 +49,16 @@ export function SearchBar({ onSubmit, term }) {
               onChange={handleChange} 
               id="search"
             />
-            <RxMagnifyingGlass className="absolute left-3 top-5 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            {/* <RxMagnifyingGlass className="absolute left-3 top-5 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
           </div> 
+          
           <button 
-            className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+            className="w-64 h-10 rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
             disabled={Input===''}
           >
               Search Picture
           </button>
+          
         </div>
       </form>
     
